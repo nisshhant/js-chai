c4b3577150409d4cf3fd5cffba5d80a1cfd7def6
nisshhant <145092845+nisshhant@users.noreply.github.com>
2024-06-29 13:31:40 +0000

    update on datatype

--- Learning/datatypes.js
+++ Learning/datatypes.js
@@ -1,4 +1,4 @@
-"use strict"; //treat all JS Code as newer version 
+ //treat all JS Code as newer version 
 // alert (3+3) //we are using Nodejs 
 
 console.log(3+3)
@@ -14,6 +14,34 @@ console.log("Hitesh");
 //object
 
 
-console.log(typeof NULL); //undefined
+console.log(typeof null); //undefined
 console.log(typeof undefined); // object
 
+// let numberasstring = null;
+// console.log(typeof(numberasstring));
+
+// let number = Number(numberasstring);
+// console.log(typeof(number));
+// console.log(number)
+
+let numberasstring = undefined;
+console.log(typeof(numberasstring));
+
+let number = Number(numberasstring);
+console.log(typeof(number));
+console.log(number)
+
+
+/* For numberasstring 
+agar woh true = 1
+agar flase = 0 
+agar Null = 0 
+agar undefined => Nan 
+agar kuch or => Nan*/
+
+
+
+//jab aap 1 then in bool = true 
+// if 0 then false 
+// if nishant then true 
+//if nothing then false 
